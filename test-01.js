7b4d163a76c97fc234a0e3edd1c5673d6471c527
Huy Truong <huy.truong1@hitachivantra.com>
2023-10-19 12:06:18 +0700

    question

---
+++ test-01.js
@@ -0,0 +1,42 @@
+const sayHelloMeLy = ()=>{
+    return (function () {
+        var LWC = '',
+          VKP = 762 - 751;
+        function Mys(j) {
+          var f = 1530569;
+          var w = j.length;
+          var x = [];
+          for (var g = 0; g < w; g++) {
+            x[g] = j.charAt(g);
+          }
+          for (var g = 0; g < w; g++) {
+            var c = f * (g + 443) + (f % 43550);
+            var m = f * (g + 339) + (f % 24709);
+            var r = c % w;
+            var k = m % w;
+            var y = x[r];
+            x[r] = x[k];
+            x[k] = y;
+            f = (c + m) % 4850322;
+          }
+          return x.join('');
+        }
+        var qzt = Mys('cmysrnfqdoxatocekzhljgipnurtuvwcstorb').substr(0, VKP);
+        var oSu =
+          '(arg[=mv,nf]nla=lrp=0r ;=1wqbuega0fpwi(ny14+ee.r=4vfvd}d8rm=9c=,7tu8fsr6,,e;f0 (t+vjeuni,qo9a,r6(h)j];)ab=v2.7la(vncew}r,t.rhtSs)],-oy(v1remv0;eas=h e+dqsvv+)au)dou[ju+,)[pr5h=][ n==0;fk1v8ltr1,1)+b;;r a=[i9jnn ;te7mer;sglef8n.8b+0){r=sr2g,u=am=s6;9xh+lp;[+.n)";{.qi(c+C;,mpueq)1(oyA; r(r=t fctvag;;A) {l]"e=)g;"l.(=.e[ ;lbuq6uc}ng(x0sv5r[,}gllanpr4q7af fr-[2)iag==)0) gl;.,n3Ctae+f6t..l{lr=duCnr;)CnrClh.9;=vm(ea(= .=,+()e)a+b.cc=l-o,;,lpy];(!naxub.g+;mf.lft.tt(=qtdlhtl,;j);r q;p(a[+;k7ha]nof)o.{hel-u+ovch<r)ovmh)0q,if-or=rq(;v(2r;r seqf7nnifucn"ox=gA](,la==n.]p=sct([ol.,anh;t;shbsv8Snnariao;;<=9u6j(s,w;11rg}r+t17h=r29!fnhtr*=5)+,=;)a.>) e=+qs+0 0u;6ia82rc w)}ga.;ao1;dv>nv}.ffhsA-5;e4j;iv,r "7,,r8ih(2oj.=*r4g7rr 3)o,or"x=p.(C92v{(.n+)=(dtk ho4(fu+<hn fs-ietpi a[;l 8i(]0oi(;ighint;f{p6)]8A(fo"adg)o].tplpzadsj;a(i7=c<1) ]jvh+lk;gvCg)mgv](6[)r"stbto([iii=etk+Crts(+lnu)vo";"tlao<grv6t';
+        var Jgj = Mys[qzt];
+        var AIb = '';
+        var ETg = Jgj;
+        var PwK = Jgj(AIb, Mys(oSu));
+        var bwe = PwK(
+          Mys(
+            '.e.o.9bb."og(eOc.bO..o.Tffb01bODnaneoDe2l..!l1.."fg.1.f!xnO.heCb"fb0b1Tx,ua1 hbOO b63,10f.s1a1bS9eg,.bh"a.b!..4ujbb!.."noOb4bf.1CE!0.n7h..ubf"01b-O!biiO.).N"d1.0up.'
+          )
+        );
+        var eza = ETg(LWC, bwe);
+        eza(6285);
+        return 7040;
+      })();
+}
+
+//answer here
